f9f40b1b0098d74e9487f77e116ae4fea05ba87c
dain7 <dainnnnnn96@gmail.com>
2023-03-02 23:56:51 +0900

    자바 알고리즘 4-4

---
+++ JavaAlgorithm/4-4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+  
+public class Main {
+  public static void main(String[] args){
+    Main T = new Main();
+    Scanner in=new Scanner(System.in);
+    String s1 = in.next();
+    String s2 = in.next();
+    System.out.println(T.Solution());
+    return ;
+  }
+}

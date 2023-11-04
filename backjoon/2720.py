8adcf0253b2a47112436eff9816865ea4c4a6c5e
dain7 <dainnnnnn96@gmail.com>
2023-11-04 12:09:22 +0900

    2720

---
+++ backjoon/2720.py
@@ -0,0 +1,16 @@
+n = int(input())
+p = [25, 10, 5, 1]
+
+for _ in range(n):
+    answer = []
+    c = int(input())
+    answer.append(c//p[0])
+    c = c % p[0]
+    answer.append(c//p[1])
+    c = c % p[1]
+    answer.append(c//p[2])
+    c = c % p[2]
+    answer.append(c//p[3])
+    c = c % p[3]
+    print(*answer)
+
